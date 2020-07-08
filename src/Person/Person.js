2660a311eb1972f534df2e2600c456ec2ec563e8
Deekshith H <hsdeekshith@gmail.com>
2020-07-08 17:00:55 +0530

    FEAT: RADIUM MEDIA QUERY

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,12 +4,12 @@ import './Person.css';
 
 const person = ( props ) => {
     const styles = {
-      'media: 450px':{
-        width: '400px'
+      '@media(min-width: 400px)': {
+        width: '450px'
       }
     }
     return (
-        <div className="Person">
+        <div className="Person" style={styles} >
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
