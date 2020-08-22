89b37046123e499ec96cf08681186e8e17e56f0f
Deekshith H <hsdeekshith@gmail.com>
2020-08-22 14:54:26 +0530

    FEAT: REACT LIFECYCLE HOOKS

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Person.css';
 
+class Person extends Component {
 
-const person = ( props ) => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  )
-};
+	// getDerivedStateFromProps(props, state){
+	// 	console.log('[Persons.js] getDerivedStateFromProps');
+	// 	return state;
+	// }
 
-export default person;
+	// componentWillReceiveProps(props){
+	// 	console.log('[Persons.js] componentWillReceiveProps', props);
+	// }
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+	}
+
+	getSnapshotBeforeUpdate(nextProps, nextState){
+		console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return true;
+	}
+
+	render(
+		return (
+    	<div className={classes.Person}>
+	      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+	      <p>{props.children}</p>
+	      <input type="text" onChange={props.changed} value={props.name} />
+	    </div>
+	  )
+	);
+}
+
+export default Person;
